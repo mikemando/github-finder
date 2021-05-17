d37fa70e4a5b143da23aa093d4527693eb6e7dd9
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2021-05-17 08:02:25 -0800

    implemented alert component, refactored search component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Search from "./components/users/Search";
 import Navbar from "./components/layout/Navbar";
 import Users from "../src/components/users/Users";
+import Alert from "./components/layout/Alert";
 import "./App.css";
 
 class App extends Component {
     state = {
         users: [],
         loading: false,
+        alert: null,
     };
 
     async componentDidMount() {
         this.setState({ loading: true });
 
-        const res = await axios.get("https://api.github.com/users");
+        const res = await axios.get(
+            `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
 
         this.setState({ users: res.data, loading: false });
     }
 
+    searchUsers = async (text) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ users: res.data.items, loading: false });
+    };
+
+    clearUsers = () => {
+        this.setState({ users: [], loading: false });
+    };
+
+    setAlert = (msg, type) => {
+        this.setState({ alert: { msg, type } });
+
+        setTimeout(() => {
+            this.setState({ alert: null });
+        }, 5000);
+    };
+
     render() {
+        const { loading, users } = this.state;
         return (
             <div className="App">
                 <Navbar title="Github Finder" />
+
                 <div className="container">
-                    <Users
-                        loading={this.state.loading}
-                        users={this.state.users}
+                    <Alert alert={this.state.alert} />
+                    <Search
+                        searchUsers={this.searchUsers}
+                        clearUsers={this.clearUsers}
+                        hasUsers={users.length > 0 ? true : false}
+                        setAlert={this.setAlert}
                     />
+                    <Users loading={loading} users={users} />
                 </div>
             </div>
         );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.props.users.map((user) => {
-                    return <UserItem key={user.id} user={user} />;
-                })}
-            </div>
-        );
+const Users = ({ loading, users }) => {
+    if (loading) {
+        <Spinner />;
     }
-}
+
+    return (
+        <div style={userStyle}>
+            {users.map((user) => {
+                return <UserItem key={user.id} user={user} />;
+            })}
+        </div>
+    );
+};
 
 const userStyle = {
     display: "grid",
     gridTemplateColumns: "repeat(3, 1fr)",
     gridGap: "1rem",
+};
+
+Users.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    users: PropTypes.array.isRequired,
 };
 
 export default Users;
