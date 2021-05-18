9cf498fd47a996317abd9b20981c85b358b95c02
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2021-05-17 16:52:23 -0800

    refactored

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Search from "./components/users/Search";
 import Navbar from "./components/layout/Navbar";
 import Users from "../src/components/users/Users";
+import User from "../src/components/users/User";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import "./App.css";
 
 class App extends Component {
     state = {
         users: [],
+        user: {},
+        repos: [],
         loading: false,
         alert: null,
     };
@@ -31,6 +36,22 @@ class App extends Component {
         this.setState({ users: res.data.items, loading: false });
     };
 
+    getUsers = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/search/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ users: res.data, loading: false });
+    };
+
+    getUserRepos = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/search/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ repos: res.data, loading: false });
+    };
+
     clearUsers = () => {
         this.setState({ users: [], loading: false });
     };
@@ -44,22 +65,51 @@ class App extends Component {
     };
 
     render() {
-        const { loading, users } = this.state;
+        const { loading, users, user } = this.state;
         return (
-            <div className="App">
-                <Navbar title="Github Finder" />
-
-                <div className="container">
-                    <Alert alert={this.state.alert} />
-                    <Search
-                        searchUsers={this.searchUsers}
-                        clearUsers={this.clearUsers}
-                        hasUsers={users.length > 0 ? true : false}
-                        setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users} />
+            <Router>
+                <div className="App">
+                    <Navbar title="Github Finder" />
+                    <div className="container">
+                        <Alert alert={this.state.alert} />
+                        <Switch>
+                            <Route
+                                exact
+                                path="/"
+                                render={(props) => (
+                                    <Fragment>
+                                        <Search
+                                            searchUsers={this.searchUsers}
+                                            clearUsers={this.clearUsers}
+                                            hasUsers={
+                                                users.length > 0 ? true : false
+                                            }
+                                            setAlert={this.setAlert}
+                                        />
+                                        <Users
+                                            loading={loading}
+                                            users={users}
+                                        />
+                                    </Fragment>
+                                )}
+                            />
+                            <Route exact path="/about" component={About} />
+                            <Route
+                                exact
+                                path="user/:login"
+                                render={(props) => (
+                                    <User
+                                        {...props}
+                                        getUser={this.getUser}
+                                        user={user}
+                                        loading={loading}
+                                    />
+                                )}
+                            />
+                        </Switch>
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Navbar = ({ title }) => {
     return (
         <nav className="navbar bg-primary">
             <h1>{title} </h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
         </nav>
     );
 };
